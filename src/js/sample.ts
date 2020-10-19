92798c6a710e3bdf8cf4b44d9ecd4d2567ba1dfd
Claire Villard <60586848+claire-villard-sonarsource@users.noreply.github.com>
2020-10-19 18:05:38 +0200

    Add complexity

--- src/js/sample.ts
+++ src/js/sample.ts
@@ -1,2 +1,16 @@
 let n: number = 1;
 alert('n is ' + n);
+
+function foo(a, b, c) {
+  var d;
+  if (a) {
+    if (b) {
+      while (c ? d=12 : d=15) {
+        if (d==12 || c==14 && a==b) {
+          return a;
+        }
+      }
+    }
+  }
+  return b;
+}
