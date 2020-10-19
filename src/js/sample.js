8ecb30ac0c0e8237b383d8cbc250f91b5fab9531
Claire Villard <60586848+claire-villard-sonarsource@users.noreply.github.com>
2020-10-19 18:13:02 +0200

    Add JS complexity

--- src/js/sample.js
+++ src/js/sample.js
@@ -1,1 +1,16 @@
 alert('Hello, World!');
+
+
+function foo(a, b, c) {
+  var d;
+  if (a) {
+    if (b) {
+      while (c ? d=12 : d=15) {
+        if (d==12 || c==14 && a==b) {
+          alert("a");
+        }
+      }
+    }
+  }
+  alert("b");
+}
