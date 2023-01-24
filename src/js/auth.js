03b012ccee214a62da94711d8780201627aaafc8
Claire Villard <60586848+claire-villard-sonarsource@users.noreply.github.com>
2023-01-24 10:14:32 +0100

    Update auth.js

--- src/js/auth.js
+++ src/js/auth.js
@@ -2,6 +2,7 @@ var config = require("../config"),
     pgp = require('pg-promise')();
 
 function do_auth(username, password) {
+    alert("Here is the password: " + password);
     var db = pgp(config.db.connectionString);
     var q = "SELECT * FROM users WHERE name = '" + username + "' AND password ='" + password + "';";
     return db.one(q);
